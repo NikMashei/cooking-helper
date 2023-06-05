263f64f65c436701838b02f5cf62d9f593a360e2
nikita.mashei <nikita.mashei@semrush.com>
2023-06-06 01:38:23 +0200

    add 'testcontainers'
    - add dependency
    - setup postgresql for tests

---
+++ src/test/kotlin/dev/mashei/cookinghelper/testcontainer/TestContainer.kt
@@ -0,0 +1,46 @@
+package dev.mashei.cookinghelper.testcontainer
+
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
+
+abstract class TestContainer {
+
+    companion object {
+
+        private val postgres: PostgreSQLContainer<*> = PostgreSQLContainer(DockerImageName.parse("postgres:13.3"))
+            .apply {
+                this.withDatabaseName("testDb")
+                    .withUsername("root")
+                    .withPassword("123456")
+            }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", Companion::jdbcUrl)
+            registry.add("spring.datasource.username", postgres::getUsername)
+            registry.add("spring.datasource.password", postgres::getPassword)
+        }
+
+        private fun jdbcUrl(): String {
+            return "jdbc:postgresql://${postgres.host}:${postgres.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT)}/${postgres.databaseName}"
+        }
+
+        @BeforeAll
+        @JvmStatic
+        fun startUp() {
+            postgres.start()
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun tearDown() {
+            postgres.stop()
+        }
+    }
+
+}
