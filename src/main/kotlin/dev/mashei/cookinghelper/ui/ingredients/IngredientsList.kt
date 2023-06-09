b70e404b3fd101dd95b5b4aa18938e43ea211e02
nikita.mashei <nikita.mashei@semrush.com>
2023-06-09 02:46:54 +0200

    add component to view ingredients

---
+++ src/main/kotlin/dev/mashei/cookinghelper/ui/ingredients/IngredientsList.kt
@@ -0,0 +1,45 @@
+package dev.mashei.cookinghelper.ui.ingredients
+
+import com.vaadin.flow.component.grid.Grid
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.data.value.ValueChangeMode
+import com.vaadin.flow.router.PageTitle
+import com.vaadin.flow.router.Route
+import dev.mashei.cookinghelper.application.IngredientsQuery
+import dev.mashei.cookinghelper.application.IngredientsQuery.*
+import dev.mashei.cookinghelper.model.Ingredient
+
+@Route("")
+@PageTitle("Ingredients | CookingHelper")
+class IngredientsList(private var ingredientsListQuery: IngredientsQuery) : VerticalLayout() {
+
+    private val grid: Grid<Ingredient> = Grid(Ingredient::class.java)
+    private val filterText: TextField = TextField()
+
+    init {
+        addClassName("ingredients-list-view")
+        setSizeFull()
+        configureFilter()
+        configureGrid()
+        add(filterText, grid)
+        updateList()
+    }
+
+    private fun updateList() {
+        grid.setItems(ingredientsListQuery.queryIngredients(Query(filterText.value)))
+    }
+
+    private fun configureFilter() {
+        filterText.placeholder = "Filter by name"
+        filterText.isClearButtonVisible = true
+        filterText.valueChangeMode = ValueChangeMode.EAGER
+        filterText.addValueChangeListener { updateList() }
+    }
+
+    private fun configureGrid() {
+        grid.addClassName("ingredient-grid")
+        grid.setSizeFull()
+        grid.setColumns("name", "unit")
+    }
+}
